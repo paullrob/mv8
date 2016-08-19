8e35ba8b7590300c9dabce85684987eeb433f826
Paul <bigwall7401@aol.com>
2016-08-19 21:40:54 +0000

    Added Home, About and Navigation Links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get "about" => "pages#about" # creates about_path
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
